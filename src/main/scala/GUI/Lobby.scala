818f12094b6dbd012a711d1b1fe08ee645fa7919
Emile Trotignon <emile.trotignon@laposte.net>
2020-02-09 20:03:08 +0100

    add text back to the button

--- src/main/scala/GUI/Lobby.scala
+++ src/main/scala/GUI/Lobby.scala
@@ -4,7 +4,8 @@ import javax.swing._
 
 class Lobby(name : String) extends JFrame(name)
 {
-    val start_game_button = new JButton("Start game")
+    val start_game_button = new JButton()
     this.add(start_game_button)
     start_game_button.setAction(StartGameAction)
+    start_game_button.setText("Start game")
 }
